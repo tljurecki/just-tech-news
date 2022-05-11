30393e8ba1a63b302b535189a9f551bda1d4f1e1
Tanya Jurecki <tljurecki@gmail.com>
2022-05-11 11:19:58 -0400

    added password hashing

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -43,6 +44,20 @@ User.init(
                 len: [4]
             }
         }
+    },
+    {
+        hooks: {
+            //set up beforeCreate lifecycle "hook" functionality
+             async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+                },
+                //set up beforeUpdate lifecycle "hook" functionality
+                async beforeUpdate(updatedUserData) {
+                    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                    return updatedUserData;
+                } 
+            }
     },
     {
         //table configureation options fo here
