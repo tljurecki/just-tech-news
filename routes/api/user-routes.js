f7b6272f3ccf4a183ea98698dbc057220a7c9676
Tanya Jurecki <tljurecki@gmail.com>
2022-05-11 14:59:40 -0400

    fixed bugs in code

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -62,6 +62,7 @@ router.post('/login', (req, res) => {
          return;
      }
      const validPassword = dbUserData.checkPassword(req.body.password);
+     
      if (!validPassword) {
          res.status(400).json({ message: 'Incorrect password' });
          return;
