438cf08867f812b283ed6d037a57260a3a0fde37
Tanya Jurecki <tljurecki@gmail.com>
2022-05-23 13:15:52 -0400

    created ability to view a single post

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Post, User, Vote, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+
+router.post('/', (req, res) => {
+    // check the session
+    if (req.session) {
+      Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
+
+  module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 //get all users
@@ -63,31 +63,16 @@ router.post('/', (req, res) => {
 });
 //Put /api/posts/upvote
 router.put('/upvote', (req, res) => {
-Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-})
-.then(() => {
-    //then find the one we just voted on
-    return Post.findOne({
-        where: {
-            id: req.body.post_id
-        },
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            //user raw MYSQL aggregate function query to get a count of how many votes the post has and return it under the na,e 'vote_count'
-            [
-                sequelize.literal('SELECT COUNT (*) FROM voteWHERE post_id = vote.post_id '),
-                'vote_count'
-            ]
-        ]
-    })
-    .then(dbPostData => res.json(dbPostData))
-.catch(err => res.json(err));
-});
+// make sure the session exists first
+if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 
